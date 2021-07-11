1bc59799a4ecd221ee9367577b9e46af7aba29b1
beakgugong <86966789+beakgugong@users.noreply.github.com>
2021-07-11 23:34:20 +0900

    Add files via upload

--- momentum/app.js
+++ momentum/app.js
@@ -3,13 +3,22 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting"); 
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onButton(event) {
     event.preventDefault();
     const username = loginInput.value;
     loginForm.classList.add(HIDDEN_CLASSNAME);
+    localStorage.setItem(USERNAME_KEY, username);
     greeting.innerText = "Hello " + username;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit",onButton);
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null ) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit",onButton);
+} else {
+    greeting.innerText = `Hello ${savedUsername}`;
+}
